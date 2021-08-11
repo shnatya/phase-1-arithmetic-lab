82a9366cb4d59a6066290f78ee09a0c910e3227a
Anastasia Shatokhina <shnatya@gmail.com>
2021-08-10 18:26:13 -0700

    commit tree aea6ce5dae25ab3bd4cc022a8324b4a39a3f6d9b

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+const num3 = 9;
+const num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(3, 5, 1, 20, 9);
